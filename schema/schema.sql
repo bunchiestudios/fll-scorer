15ea9d01505af93d53920d26def09570bdae1b89
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-01-23 20:10:17 -0600

    Finished schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,42 +1,73 @@
-INSERT TABLE IF NOT EXISTS Users (
-    INTEGER id,
-    VARCHAR name,
-    VARCHAR email,
-    TINYINT admin
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Users')
+CREATE TABLE Privileges (
+    id INTEGER IDENTITY(1, 1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-INSERT TABLE IF NOT EXISTS Events (
-    INTEGER id,
-    DATE date_start,
-    DATE date_end,
-    VARCHAR name
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Users')
+CREATE TABLE  Users (
+    id INTEGER IDENTITY(1, 1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    admin TINYINT NOT NULL
 );
 
-INSERT TABLE IF NOT EXISTS Teams (
-    INTEGER team_number,
-    VARCHAR name
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Events')
+CREATE TABLE  Events (
+    id INTEGER IDENTITY(1, 1) PRIMARY KEY,
+    year INTEGER NOT NULL,
+    date_start DATE NOT NULL,
+    date_end DATE NOT NULL,
+
+    name VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (year) REFERENCES Seasons(year)
 );
 
-INSERT TABLE IF NOT EXISTS Seasons (
-    INTEGER year,
-    VARCHAR schema
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Teams')
+CREATE TABLE  Teams (
+    team_number INTEGER PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-INSERT TABLE IF NOT EXISTS Scorekeepers (
-
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Seasons')
+CREATE TABLE  Seasons (
+    year INTEGER PRIMARY KEY,
+    json_schema VARCHAR(MAX) NOT NULL
 );
 
-INSERT TABLE IF NOT EXISTS Enrollments (
-    INTEGER team_number,
-    INTEGER event_id,
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Scorekeepers')
+CREATE TABLE  Scorekeepers (
+    user_id INTEGER NOT NULL,
+    event_id INTEGER NOT NULL,
+    level_id TINYINT NOT NULL,
 
-    VARCHAR practice_data,
-    VARCHAR match1_data,
-    VARCHAR match2_data,
-    VARCHAR match3_data,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (event_id) REFERENCES Events(id),
+    PRIMARY KEY (user_id, event_id)
+);
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name='Enrollments')
+CREATE TABLE  Enrollments (
+    team_number INTEGER NOT NULL,
+    event_id INTEGER NOT NULL,
+
+    practice_data VARCHAR(MAX),
+    match1_data VARCHAR(MAX),
+    match2_data VARCHAR(MAX),
+    match3_data VARCHAR(MAX),
     
-    TINYINT practice_noshow,
-    TINYINT match1_noshow,
-    TINYINT match2_noshow,
-    TINYINT match3_noshow
+    practice_noshow TINYINT,
+    match1_noshow TINYINT,
+    match2_noshow TINYINT,
+    match3_noshow TINYINT,
+
+    FOREIGN KEY (team_number) REFERENCES Teams(team_number),
+    FOREIGN KEY (event_id) REFERENCES Events(id),
+    PRIMARY KEY (team_number, event_id)
 );
+
+IF NOT EXISTS (SELECT * FROM Privileges WHERE name = 'admin')
+    INSERT INTO Privileges (name) VALUES ('admin')
+
+IF NOT EXISTS (SELECT * FROM Privileges WHERE name = 'editor')
+    INSERT INTO Privileges (name) VALUES ('editor')
